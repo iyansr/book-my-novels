2da4243b6bcb8afaf07cb1fd13a8f2212f904daf
IyanSR <ikhyan7@gmail.com>
2019-11-06 18:11:43 +0700

    Refactor carousel

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -4,19 +4,21 @@ import './Carousel.css';
 import books from '../../Helpers/books';
 
 const Carousel = () => {
+  const carouselMap = books.map((book, index) => {
+    return (
+      <CarouselCard
+        alt={book.title.trim()}
+        key={index}
+        author={book.author}
+        title={book.title}
+        img={book.image_url}
+      />
+    );
+  });
+
   return (
     <div className='carousel' id='myCarousel'>
-      {books.map((book, index) => {
-        return (
-          <CarouselCard
-            alt={book.title.trim()}
-            key={index}
-            author={book.author}
-            title={book.title}
-            img={book.image_url}
-          />
-        );
-      })}
+      {carouselMap}
     </div>
   );
 };
