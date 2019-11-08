c0553e2cf8ed4b43c90c43aab16ea12213ebcbc8
IyanSR <ikhyan7@gmail.com>
2019-11-08 21:29:35 +0700

    Create Data Working

--- src/Components/Modal/AddModal.js
+++ src/Components/Modal/AddModal.js
@@ -1,179 +1,123 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class AddModal extends Component {
-  constructor() {
-    super();
-    this.state = {
-      book: {
-        title: '',
-        author: '',
-        image_url: '',
-        date: '',
-        year: '',
-        description: '',
-        status: ''
-      }
-    };
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+const AddModal = props => {
+  const {
+    title,
+    author,
+    image_url,
+    date,
+    year,
+    description,
+    status,
+    onChange,
+    onSubmit
+  } = props;
+  return (
+    <div id='addNovelModal' className='modal'>
+      <div className='modal-content'>
+        <h4>Add Novel</h4>
+        <div className='row'>
+          <form onSubmit={onSubmit}>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>person</i>
+              <input
+                name='title'
+                id='title'
+                type='text'
+                className='validate'
+                placeholder='Title'
+                value={title}
+                onChange={onChange}
+              />
+            </div>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>person</i>
+              <input
+                name='author'
+                id='author'
+                type='text'
+                className='validate'
+                placeholder='Author'
+                value={author}
+                onChange={onChange}
+              />
+            </div>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>account_circle</i>
+              <input
+                name='image_url'
+                id='image_url'
+                type='text'
+                className='validate'
+                placeholder='Image URL'
+                value={image_url}
+                onChange={onChange}
+              />
+            </div>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>account_circle</i>
+              <input
+                name='date'
+                id='date'
+                type='text'
+                className='validate'
+                placeholder='Date'
+                value={date}
+                onChange={onChange}
+              />
+            </div>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>account_circle</i>
+              <input
+                name='status'
+                id='status'
+                type='text'
+                className='validate'
+                placeholder='Status'
+                value={status}
+                onChange={onChange}
+              />
+            </div>
 
-  handleChange = e => {
-    const { name, value } = e.target;
-
-    this.setState({
-      book: { ...this.state.book, [name]: value }
-    });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-    const {
-      title,
-      author,
-      image_url,
-      date,
-      year,
-      description,
-      status
-    } = this.state.book;
-
-    const { addNovel } = this.props;
-
-    const newNovel = {
-      title,
-      author,
-      image_url,
-      date,
-      year,
-      description,
-      status
-    };
-
-    addNovel(newNovel);
-    this.setState(this.intialState);
-    console.log('modal onsubmit');
-    console.log(this.state.book);
-  };
-
-  render() {
-    const {
-      title,
-      author,
-      image_url,
-      date,
-      year,
-      description,
-      status
-    } = this.state.book;
-    return (
-      <div id='addNovelModal' className='modal'>
-        <div className='modal-content'>
-          <h4>Add Novel</h4>
-          <div className='row'>
-            <form>
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>person</i>
-                <input
-                  name='title'
-                  id='title'
-                  type='text'
-                  className='validate'
-                  placeholder='Title'
-                  value={title}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>person</i>
-                <input
-                  name='author'
-                  id='author'
-                  type='text'
-                  className='validate'
-                  placeholder='Author'
-                  value={author}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>account_circle</i>
-                <input
-                  name='image_url'
-                  id='image_url'
-                  type='text'
-                  className='validate'
-                  placeholder='Image URL'
-                  value={image_url}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>account_circle</i>
-                <input
-                  name='date'
-                  id='date'
-                  type='text'
-                  className='validate'
-                  placeholder='Date'
-                  value={date}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>account_circle</i>
-                <input
-                  name='status'
-                  id='status'
-                  type='text'
-                  className='validate'
-                  placeholder='Status'
-                  value={status}
-                  onChange={this.handleChange}
-                />
-              </div>
-
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>account_circle</i>
-                <input
-                  name='year'
-                  id='year'
-                  type='text'
-                  className='validate'
-                  placeholder='Year'
-                  value={year}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className='input-field col m12'>
-                <i className='material-icons prefix'>account_circle</i>
-                <textarea
-                  name='description'
-                  id='description'
-                  type='text'
-                  className='materialize-textarea'
-                  placeholder='Description'
-                  value={description}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </form>
-          </div>
-        </div>
-        <div className='modal-footer'>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>account_circle</i>
+              <input
+                name='year'
+                id='year'
+                type='text'
+                className='validate'
+                placeholder='Year'
+                value={year}
+                onChange={onChange}
+              />
+            </div>
+            <div className='input-field col m12'>
+              <i className='material-icons prefix'>account_circle</i>
+              <textarea
+                name='description'
+                id='description'
+                type='text'
+                className='materialize-textarea'
+                placeholder='Description'
+                value={description}
+                onChange={onChange}
+              />
+            </div>
+            <div className='modal-footer'>
+              <button type='submit' className='btn waves-effect waves-light'>
+                Save
+              </button>
+            </div>
+          </form>
           <button
             className='modal-close btn-flat yellow'
             style={{ marginRight: '20px' }}>
             Close
           </button>
-          <button
-            onClick={this.onSubmit}
-            className='btn waves-effect waves-light'>
-            Save
-          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default AddModal;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import NaviBar from '../Components/Navbar/Navbar';
-import Carousel from '../Components/Carousel/Carousel';
 import Cards from '../Components/Cards/Cards';
 import Footer from '../Components/Footer/Footer';
 import books from '../Helpers/books';
 import AddModal from '../Components/Modal/AddModal';
+import CarouselCard from '../Components/Carousel/Card';
+import '../Components/Carousel/Carousel.css';
 
 import M from 'materialize-css';
 
@@ -12,8 +13,18 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      book: [...books]
+      book: [...books],
+      tempBook: {
+        title: '',
+        author: '',
+        image_url: '',
+        date: '',
+        year: '',
+        description: '',
+        status: ''
+      }
     };
+    this.onClick = this.onClick.bind(this);
   }
   componentDidMount() {
     M.AutoInit();
@@ -25,10 +36,48 @@ class Home extends Component {
     M.Carousel.init(elems, options);
   }
 
-  addNovel = novel => {
-    const { book } = this.state;
+  handleChange = e => {
+    const { name, value } = e.target;
 
-    this.setState({ book: [novel, ...book] });
+    this.setState({
+      tempBook: { ...this.state.tempBook, [name]: value }
+    });
+  };
+
+  onClick = e => {
+    e.preventDefault();
+    const {
+      title,
+      author,
+      image_url,
+      date,
+      year,
+      description,
+      status
+    } = this.state.tempBook;
+
+    const newNovel = {
+      title,
+      author,
+      image_url,
+      date,
+      year,
+      description,
+      status
+    };
+
+    this.setState({
+      book: [...this.state.book, newNovel]
+    });
+
+    const elems = document.querySelectorAll('.carousel');
+    const options = {
+      duration: 100
+    };
+    M.Carousel.init(elems, options);
+
+    console.log('onclick');
+    console.log(this.state.book);
   };
 
   render() {
@@ -36,9 +85,21 @@ class Home extends Component {
 
     return (
       <div className='home-page'>
-        <AddModal addNovel={this.addNovel} />
         <NaviBar />
-        <Carousel />
+
+        <div className='carousel' id='myCarousel'>
+          {this.state.book.map((novel, index) => {
+            return (
+              <CarouselCard
+                alt={novel.title.trim()}
+                key={index}
+                author={novel.author}
+                title={novel.title}
+                img={novel.image_url}
+              />
+            );
+          })}
+        </div>
         <div className='container'>
           <h4
             style={{
@@ -48,7 +109,7 @@ class Home extends Component {
             List Novels
           </h4>
           <div className='row'>
-            {books.map((book, index) => {
+            {this.state.book.map((book, index) => {
               return (
                 <Cards
                   alt={book.title.trim()}
@@ -62,6 +123,17 @@ class Home extends Component {
             })}
           </div>
         </div>
+        <AddModal
+          title={this.state.tempBook.title}
+          author={this.state.tempBook.author}
+          image_url={this.state.tempBook.image_url}
+          date={this.state.tempBook.date}
+          year={this.state.tempBook.year}
+          description={this.state.tempBook.description}
+          status={this.state.tempBook.status}
+          onChange={this.handleChange}
+          onSubmit={this.onClick}
+        />
         <Footer />
       </div>
     );
