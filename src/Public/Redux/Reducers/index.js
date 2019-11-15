62c4b34628f3a50b79d516f0aeb6d896599ce306
IyanSR <ikhyan7@gmail.com>
2019-11-15 11:24:25 +0700

    Update UI

--- src/Public/Redux/Reducers/index.js
+++ src/Public/Redux/Reducers/index.js
@@ -5,13 +5,13 @@ import genres from './genres';
 import status from './status';
 
 const appReducer = combineReducers({
-  novels: novels.getNovels,
-  novelsId: novels.getNovelsId,
-  postNovel: novels.postNovel,
-  editNovel: novels.editNovel,
-  deleteNovel: novels.deleteNovel ,
-  genres: genres,
-  status
+	novels: novels.getNovels,
+	// novelsId: novels.getNovelsId,
+	postNovel: novels.postNovel,
+	editNovel: novels.editNovel,
+	deleteNovel: novels.deleteNovel,
+	genres: genres,
+	status,
 });
 
 export default appReducer;
